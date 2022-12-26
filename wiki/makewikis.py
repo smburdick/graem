1f377f7258efb4b98394cdc3e426237d2481737b
Sam Burdick <burdick.52@osu.edu>
2022-12-26 13:40:43 -0500

    update script for new article name fmt

--- wiki/makewikis.py
+++ wiki/makewikis.py
@@ -2,14 +2,14 @@ from sys import argv
 from os import mkdir
 from datetime import date
 
-firstLetterUpper = lambda s: s[0].upper() + s[1:]
+title_fmt = lambda s: (s[0].upper() + s[1:]).replace("_", " ")
 
 for name in argv[1:]:
     try:
         mkdir(name)
         with open(name + "/index.md", "w+") as file:
             file.write("---\n")
-            file.write("title: \"{}\"\n".format(firstLetterUpper(name)))
+            file.write("title: \"{}\"\n".format(title_fmt(name)))
             file.write("date: {}\n".format(date.today().strftime("%Y-%m-%d")))
             file.write("description: \"\"\n")
             file.write("tags: \n")
